835f25eb910ddbe305aa336137dbbb4c8b3385fa
Maria <mariaf7458@gmail.com>
2023-04-30 23:35:05 -0500

    Se agrego funcionalidad a boton analizar

--- main.js
+++ main.js
@@ -1,81 +1,197 @@
 //     input usario
 
+
 function analisisPersonalSalario () {
 
-    //años
-    let inputAño = document.getElementById("año").value;
-    console.log(inputAño);
-    let separadoA = inputAño.split(",");
-    let añosTrabajo = [];
+    //agregar boton limpiar
+    document.getElementById('limpio').classList.remove('inactive');
 
-    separadoA.forEach(element => {
-        añosTrabajo.push(Number(element));
-    });
-    
+
+
+    //Resultado div
+    document.getElementById('procesos').classList.remove('inactive')
+    document.querySelector('.btnAnalizar').classList.add('inactive')
+
+
+    //titulos
+    const historial = document.createElement('span');
+    document.querySelector('#historialSalarios').appendChild(historial);
+    historial.innerText ='Historial Salarios';
+
+    //Historial empleos
+
+
+    //años 
+    const añosTrabajo = Analisis.ingresoAños();
     console.log(añosTrabajo);
 
     //trabajo
-    let inputTrabajo = document.getElementById('trabajo').value;
-    console.log(inputTrabajo);
-    let trabajos = inputTrabajo.split(",");
+    const trabajos = Analisis.ingresoTrabajos();
     console.log(trabajos);
 
     //salario
+    
+
+    let añoSalarios = Analisis.ingresoSalarios();
+
+
+
+
+    //salarios string
+
     let inputSalario = document.getElementById("salario").value;
-    console.log(inputSalario);
-    let separadoS = inputSalario.split(",");
+      let salarioString = inputSalario.split(",");
+    console.log(salarioString);
 
-    let añoSalarios = [];
+    let salarioDecimal = [];
 
-    separadoS.forEach(element => {
-        añoSalarios.push(Number(element));
+    salarioString.forEach(element => {
+        salarioDecimal.push(parseFloat(element).toFixed(6));
     });
 
     console.log(añoSalarios);
+    console.log(salarioDecimal);
+    //promedio salarios
+    const promedioSalarios = Analisis.calcularPromedio(añoSalarios);
+    console.log(promedioSalarios);
 
-    //mediana salarios
-    const medianaSalarios = Analisis.calcularMediana(añoSalarios);
+    //moda salarios
+    const modaSalarios = Analisis.calcularModa(añoSalarios);
+    console.log(modaSalarios);
+
+
+    //Mediana salarios
+    const medianaSalarios = Analisis.medianaSalarios(añoSalarios);
     console.log(medianaSalarios);
 
     //proyeccion 
-    //porcentajes crecimiento por años
-    let porcentajesCrecimiento = [];
 
-    for (let i = 1; i < trabajos.length; i++) {
-        const salarioActual = añoSalarios[i];
-        const salarioPasado = añoSalarios [i -1];
-        const crecimiento = salarioActual - salarioPasado;
-        const porcentajeCrecimiento = crecimiento / salarioPasado;
-        porcentajesCrecimiento.push(porcentajeCrecimiento)
-    }
+    //porcentajes crecimiento por años
+    const porcentajesCrecimiento = Analisis.porcentajesCrecimiento(trabajos, añoSalarios);
 
     console.log(porcentajesCrecimiento);
 
     //Mediana porcentajes crecimiento
-
     const medianaPorcentajesCrecimiento = Analisis.calcularMediana(porcentajesCrecimiento) 
     console.log(medianaPorcentajesCrecimiento);
 
     // Proyeccion nuevo salario
-
-    ultimoSalario = añoSalarios.length - 1;
-    const aumentoSalario = ultimoSalario * medianaPorcentajesCrecimiento;
-    const nuevoSalario = ultimoSalario + aumentoSalario;
+    const nuevoSalario = Analisis.nuevoSalario(añoSalarios, medianaPorcentajesCrecimiento)
 
     console.log(nuevoSalario);
 
     //Top salarios
-    let topSalarios = [];
-
-    for (let i = 1; i < trabajos.length; i++) {
-        const salarioActual = añoSalarios[i];
-        const salarioPasado = añoSalarios [i -1];
-        if(salarioActual > salarioPasado){
-            topSalarios.push(salarioActual)
-        }
-    }
+    const topSalarios= Analisis.topMejoreSalarios(añoSalarios);
     console.log(topSalarios);
+
+
+    //salarios default order
+    function defecto () {
+        return 0;
+    }
+
+    //Historial salarios
+    
+
+    const ctx = document.getElementById('salariosCanvas').getContext('2d');
+
+    //gradient fill
+    let gradient = ctx.createLinearGradient(0,0,0,400);
+    gradient.addColorStop(0,'rgb(255, 127, 0, 1');
+    gradient.addColorStop(1, 'rgb(255,20,147, 0.3')
+
+    const labels = añosTrabajo;
+
+    const data = {
+        labels,
+        datasets: [
+            { 
+            data: salarioString,
+            label: "Salarios a traves del tiempo",
+            fill: true,
+            backgroundColor: gradient,
+        },
+    ],
    
+    };
+
+    const  config = {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    ticks: {
+                        callback: function (value) {
+                            return value + "$";
+                        }
+                    }
+                },
+            },
+        },
+    };
+    const myChart = new Chart (ctx, config);
+
+
+    //Promedio salarios
+    const promedioSalarioString = promedioSalarios.toString(promedioSalarios);
+    const promedio = document.createElement('span');
+    const promedioTitulo = document.createElement('span');
+    document.querySelector('.promedioTitulo').appendChild(promedioTitulo);
+    document.querySelector('.promedioSalarios').appendChild(promedio);
+
+    promedioTitulo.innerText = 'Promedio Salarios';
+    promedio.innerText = promedioSalarioString;
+
+
+    //top salarios
+    const topSalarioString = topSalarios.toString(topSalarios);
+    const topS = document.createElement('span');
+    document.querySelector('#topSalarioS').appendChild(topS);
+    topS.innerText ='Top Salarios' + '\n';
+    const top1S = topSalarios[0].toString();
+    const top2S = topSalarios[1].toString();
+    const top3S = topSalarios[2].toString();
+    const tops = document.createElement('span');
+    document.querySelector('#topsLista').appendChild(tops);
+    tops.innerText = '• '+top1S + '\n' + '• ' + top2S + '\n' + '• ' + top3S;
+
+
+
+    //Porcentajes Crecimiento
+ 
+
+
+    /*const porcent = document.querySelector('#crecimientoP');
+
+    const labelsP = añosTrabajo;
+
+    const dataP = {
+        labelsP,
+        datasets: [
+            {
+                data: porcentajesCrecimiento,
+                label: 'Porcentajes Crecimiento',
+        }]
+    }
+
+    const configP = {
+        type: 'line',
+        data: dataP,
+        options: {
+            responsive: true,
+        },
+    };
+
+    const myChartP = new Chart(porcent, configP)
+    */
+
+    //media porcentaje crecimiento
+    const medianaPS = medianaPorcentajesCrecimiento.toString();
+    document.getElementById('mediapc').innerText = medianaPS; 
 }
 
-
+function limpiar () {
+    location.reload();
+}
